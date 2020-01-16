ec21d9dbd98313d32846d1d5ae051fd5d2ae4b62
Slopez <sloppy@sloppylopez.com>
2020-01-17 00:03:15 +0100

    Added unit test for authores.component

--- src/app/authors/authors.component.spec.ts
+++ src/app/authors/authors.component.spec.ts
@@ -1,10 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { AuthorsComponent } from './authors.component';
 import {AuthorService} from "./authors.service";
 
 describe('AuthorsComponent', () => {
   let component: AuthorsComponent;
+  let authorService: AuthorService;
+  let spy: jasmine.Spy;
+  let de: DebugElement;
+  let el: HTMLElement;
   let fixture: ComponentFixture<AuthorsComponent>;
 
   beforeEach(async(() => {
@@ -27,9 +32,23 @@ describe('AuthorsComponent', () => {
     fixture = TestBed.createComponent(AuthorsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    authorService = fixture.debugElement.injector.get(AuthorService);
+    const authors = [{ name: 'author1', book: 'book1' }];
+    spy = spyOn(authorService, 'getAuthors')
+      .and.returnValue(Promise.resolve(authors));
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      de = fixture.debugElement.query(By.css('.list-group-item'));
+      el = de.nativeElement;
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render author list', () => {
+    expect(el.textContent.trim()).toBe('Author1');
   });
 });
